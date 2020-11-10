98a6f6dee23e70940d116c207400b332e7b3413d
JH <hanj527@newschool.edu>
2020-11-10 14:57:48 +0000

    update

---
+++ week09/dht.ino
@@ -0,0 +1,79 @@
+#include "Adafruit_DHT.h"
+
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+#define DHTPIN 5     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11		// DHT 11 
+#define DHTTYPE DHT22		// DHT 22 (AM2302)
+//#define DHTTYPE DHT21		// DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+double tempF = 0; 
+double humiH = 0; 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+	Serial.begin(9600); 
+	Serial.println("DHTxx test!");
+	Particle.variable("temp", tempF);
+	Particle.variable("humi", humiH );
+
+
+	dht.begin();
+	delay(5000);
+}
+
+void loop() {
+// Wait a few seconds between measurements.
+	delay(10000);
+
+// Reading temperature or humidity takes about 250 milliseconds!
+// Sensor readings may also be up to 2 seconds 'old' (its a 
+// very slow sensor)
+	float h = dht.getHumidity();
+// Read temperature as Celsius
+	float t = dht.getTempCelcius();
+// Read temperature as Farenheit
+	float f = dht.getTempFarenheit();
+	
+	tempF = f;
+	humiH = h;
+  
+// Check if any reads failed and exit early (to try again).
+	if (isnan(h) || isnan(t) || isnan(f)) {
+		Serial.println("Failed to read from DHT sensor!");
+		return;
+	}
+
+// Compute heat index
+// Must send in temp in Fahrenheit!
+	float hi = dht.getHeatIndex();
+	float dp = dht.getDewPoint();
+	float k = dht.getTempKelvin();
+
+	Serial.print("Humid: "); 
+	Serial.print(h);
+	Serial.print("% - ");
+	Serial.print("Temp: "); 
+	Serial.print(t);
+	Serial.print("*C ");
+	Serial.print(f);
+	Serial.print("*F ");
+	Serial.print(k);
+	Serial.print("*K - ");
+	Serial.print("DewP: ");
+	Serial.print(dp);
+	Serial.print("*C - ");
+	Serial.print("HeatI: ");
+	Serial.print(hi);
+	Serial.println("*C");
+	Serial.println(Time.timeStr());
+	Particle.publish("readings", String::format("{\"Hum(\%)\": %4.2f, \"Temp(°C)\": %4.2f, \"DP(°C)\": %4.2f, \"HI(°C)\": %4.2f}", h, t, dp, hi));
+}
